730eaa32eb1e90abe3abaceda395c71158abc02a
TAMUC-SNL <TAMUC-SNL@users.noreply.github.com>
2014-03-06 10:50:49 -0600

    Update EventAnalyzer.java

--- EventAnalyzer.java
+++ EventAnalyzer.java
@@ -25,14 +25,14 @@ import org.apache.hadoop.mapred.lib.MultipleTextOutputFormat;
 * Version:	1.0
 *		
 * Author:	Ilhwan Moon	
-*			Jinoh Kim	
-*			ICEL (Intelligent Cyberspace Engineering Lab)
+*		Jinoh Kim	
+*		ICEL (Intelligent Cyberspace Engineering Lab)
 *
 * This file is part of ETRI project.
 *
 ----------------------------------------------------------
 * Copyright (c) 2013-2014 SNL(System and Networks Lab)
-* Department of Computer Science, Texas A&M-Commerce
+* Department of Computer Science, Texas A&M University-Commerce
 * All Right Reserved.
 ----------------------------------------------------------
 */
